d5ac65ad87e5d85f7d3e8cf4dad007da36cc98f0
Brian Chen <bzchen93@gmail.com>
2022-10-10 21:26:50 +1100

    start queries

--- src/pullRequest.js
+++ src/pullRequest.js
@@ -1,6 +1,14 @@
 // @flow
+import type { PullRequestFilesT } from './types';
+
 const Router = require('koa-router');
-const { request } = require('@octokit/request');
+const { Octokit } = require('@octokit/rest');
+
+const repoRequestBase = {
+  owner: 'flow-typed',
+  repo: 'flow-typed',
+  branch: 'main',
+};
 
 module.exports = (router: Router) => {
   router.get('/pull-request/:prId', async (ctx) => {
@@ -10,12 +18,55 @@ module.exports = (router: Router) => {
       // return error
     }
 
-    const pr = await request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
-      owner: 'flow-typed',
-      repo: 'flow-typed',
+    const octokit = new Octokit({
+      auth: process.env.GITHUB_TOKEN,
+    });
+
+    const { data }: PullRequestFilesT = await octokit.request('GET /repos/{owner}/{repo}/pulls/{pull_number}/files', {
+      ...repoRequestBase,
       pull_number: prId,
     });
 
-    ctx.body = pr;
+    const codeowners = [];
+    data.map(async (o) => {
+      const { filename } = o;
+
+      const definitionPathStart = 'definitions/npm/';
+
+      if (filename.startsWith(definitionPathStart)) {
+        const definitionPath = filename.substring(definitionPathStart.length);
+
+        // If it's inside a scope
+        if (definitionPath.startsWith('@')) {
+          const scopePath = definitionPath.substring(0, definitionPath.indexOf('/') + 1);
+          console.log(scopePath);
+
+          try {
+            const scopeCodeowners = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+              ...repoRequestBase,
+              path: `${definitionPathStart}${scopePath}CODEOWNERS`,
+            });
+            console.log(scopeCodeowners);
+
+            // scoped lib path
+          } catch (e) {
+            //
+          }
+        } else {
+          try {
+            const lib = definitionPath.substring(0, definitionPath.indexOf('/') + 1);
+            const defCodeowners = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+              ...repoRequestBase,
+              path: `${definitionPathStart}${lib}CODEOWNERS`,
+            });
+            console.log(defCodeowners);
+          } catch (e) {
+            //
+          }
+        }
+      }
+    });
+
+    ctx.body = 'Ok';
   });
 };
--- src/types.js
+++ src/types.js
@@ -1,38 +1,6 @@
 // @flow
-export type PullRequestT = {
-  action: | 'assigned'
-    | 'auto_merge_disabled'
-    | 'auto_merge_enabled'
-    | 'closed'
-    | 'converted_to_draft'
-    | 'edited'
-    | 'labeled'
-    | 'locked'
-    | 'opened'
-    | 'ready_for_review'
-    | 'reopened'
-    | 'review_request_removed'
-    | 'review_requested'
-    | 'synchronize'
-    | 'unassigned'
-    | 'unlabeled'
-    | 'unlocked',
-  /**
-   * The pull request number.
-   */
-  number: number,
-  /**
-   * The changes to the comment if the action was edited.
-   */
-  changes: { [key: string]: any },
-  /**
-   * The previous version of the title if the action was edited.
-   */
-  pull_request: {
-    url: string,
-    id: number,
-    title: string,
-    body: string,
-
-  },
+export type PullRequestFilesT = {
+  data: Array<{
+    filename: string,
+  }>,
 };
